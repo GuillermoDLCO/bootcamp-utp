995d018551addc981be9e8d0bfacf0be537df0e5
Guillermo De La Cruz <guillermo.dlco@outlook.com>
2021-06-27 11:38:40 -0500

    Fonts implementation

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,9 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: 'Poppins',
       ),
-      home: HomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }
